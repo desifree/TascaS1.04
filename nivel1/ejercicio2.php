1316123fcc7b3d1f71b99f5927f03f2946c1321a
desifree <desireecarol@gmail.com>
2026-02-11 12:56:00 +0100

    Ejercicio 2 completado

--- nivel1/ejercicio2.php
+++ nivel1/ejercicio2.php
@@ -1,3 +1,35 @@
 <?php
+class Shape
+{
+    public $base;
+    public $height;
 
-?>
+    public function __construct($base, $height)
+    {
+        $this->base = $base;
+        $this->height = $height;
+    }
+}
+
+// subclases: rectángulo y triángulo
+class Rectangle extends Shape
+{
+    public function area()
+    {
+        return $this->base * $this->height;
+    }
+}
+
+class Triangle extends Shape
+{
+    public function area()
+    {
+        return ($this->base * $this->height) / 2;
+    }
+}
+// imprensión terminal
+$triangle = new Triangle(30, 5);
+$rectangle = new Rectangle(30, 5);
+
+echo "Area of ​Triangle: " . $triangle->area() . " cm²" . PHP_EOL;
+echo "Area of R​ectangle: " . $rectangle->area() . " cm²" . PHP_EOL;
